e8600533df4b619d2dabb908478186c72d967ae5
thecornisians <thecornisians@gmail.com>
2023-01-27 11:59:56 +0000

    created product card

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import './App.css'
 import { ExampleAtom } from './components'
 
 function App() {
-  return (
-    <div className="App">
-      <h2>App entry point</h2>
-      <ExampleAtom />
-    </div>
-  )
+    return (
+        <div className="App">
+            <h2>App entry point</h2>
+            <ExampleAtom />
+        </div>
+    )
 }
 
 export default App
--- src/components/molecules/ProductCard/ProductCard.tsx
+++ src/components/molecules/ProductCard/ProductCard.tsx
@@ -1,20 +1,16 @@
+import styles from './ProductCard.module.scss'
+
 export function ProductCard() {
-    {
-        /* w className="ProductCard"e have a card that holds a product image */
-    }
-    {
-        /* It has the product name  
-            {/* Price */
-    }
-    {
-        /* A counter to add and subtract product image*/
-    }
     return (
-        <div className="productCard">
-            <img src="https://source.unsplash.com/C6oPXOatFD8" alt="" />
-            <div>
-                <div className="rating">xxx</div>
-                <div className="price">$9999</div>
+        <div className={styles.product_card}>
+            <img
+                src="https://source.unsplash.com/C6oPXOatFD8"
+                className={styles.product_image}
+                alt=""
+            />
+            <div className={styles.price_rating}>
+                <div className={styles.rating}>xxx</div>
+                <div className={styles.price}>$9999</div>
             </div>
             <h4>Product Name</h4>
             <p>
