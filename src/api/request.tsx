7f6875312d8496fa46b3e3babc3ea5531510aee1
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-02-19 10:32:24 -0500

    Update request.tsx
    - Template'd request's as function.
    - Made a function that takes returns request function based on string/enum passed.

--- src/api/request.tsx
+++ src/api/request.tsx
@@ -1,9 +1,56 @@
 import axios from 'axios'
 
+interface REQUEST_METHOD {
+    GET: String
+    POST: String
+    PUT: String
+    DELETE: String
+}
+
+export const REQUEST_METHOD = {
+    GET: 'get',
+    POST: 'post',
+    PUT: 'put',
+    DELETE: 'delete',
+}
+
+export const REQUEST = {
+    [REQUEST_METHOD.GET]: getRequest,
+    [REQUEST_METHOD.POST]: postRequest,
+    [REQUEST_METHOD.PUT]: putRequest,
+    [REQUEST_METHOD.DELETE]: deleteRequest,
+}
+
+const apiURL = 'http://localhost:3000/'
+
 export async function getRequest(endpoint = '', headers = {}) {
-    const url = process.env.REACT_APP_API_URL + endpoint
+    const url = apiURL + endpoint
 
     const result = await axios.get(url, headers)
+
+    return result.data
+}
+
+export async function postRequest(endpoint = '', headers = {}) {
+    const url = apiURL + endpoint
+
+    const result = await axios.post(url, headers)
+
+    return result.data
+}
+
+export async function putRequest(endpoint = '', headers = {}) {
+    const url = apiURL + endpoint
+
+    const result = await axios.put(url, headers)
+
+    return result.data
+}
+
+export async function deleteRequest(endpoint = '', headers = {}) {
+    const url = apiURL + endpoint
+
+    const result = await axios.delete(url, headers)
 
     return result.data
 }
